c0802d140e77f48b8f966d2e8dfca9c3d8ecc6f1
CaptainCronch <crisdelpedro@gmail.com>
2023-06-08 22:15:48 -0600

    added basic explosive launcher

---
+++ Assets/World/level_UdonProgramSources/Rocket.cs
@@ -0,0 +1,37 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Rocket : UdonSharpBehaviour
+{
+    public GameObject explosion;
+    private Rigidbody rb;
+    private Collider col;
+
+    private float despawnTimer = 0f;
+
+    public float speed = 15f;
+    public float despawnTime = 5f;
+
+    void Start()
+    {
+        col = GetComponent<Collider>();
+        rb = GetComponent<Rigidbody>();
+        rb.AddRelativeForce(Vector3.forward * speed, ForceMode.Impulse);
+    }
+
+    void Update(){
+        if (despawnTimer >= despawnTime){
+            Destroy(gameObject);
+        } else {
+            despawnTimer += Time.deltaTime;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision){
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/World/level_UdonProgramSources/RocketLauncher.cs
@@ -0,0 +1,68 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class RocketLauncher : UdonSharpBehaviour
+{
+    public GameObject rocket;
+    public Transform muzzle;
+    public Text display;
+
+    public int maxClip = 4;
+    public float delayTime = 0.3f, reloadTime = 0.7f;
+
+    private int clip = 0;
+    private float delayTimer = 0f, reloadTimer = 0f;
+
+    private bool shooting = false;
+
+    void Start()
+    {
+        clip = maxClip;
+    }
+
+    void Update()
+    {
+        if (delayTimer < delayTime)
+        {
+            delayTimer += Time.deltaTime;
+        } 
+        else if (reloadTimer < reloadTime)
+        {
+            reloadTimer += Time.deltaTime;
+        }
+        else if (reloadTimer >= reloadTime && clip < maxClip)
+        {
+            reloadTimer = 0f;
+            clip++;
+        }
+
+        display.text = clip + "/" + maxClip;
+
+        if (shooting) {Shoot();}
+    }
+
+    public override void OnPickupUseDown()
+    {
+        shooting = true;
+    }
+
+    public override void OnPickupUseUp()
+    {
+        shooting = false;
+    }
+
+    private void Shoot()
+    {
+        if (delayTimer >= delayTime && clip > 0)
+        {
+            delayTimer = 0f;
+            reloadTimer = 0f;
+            clip--;
+            Instantiate(rocket, muzzle.position, muzzle.rotation);
+        }
+    }
+}
