8d8681a5e322bc6e575d3d6e1dd0c46c13007ceb
CaptainCronch <62529272+CaptainCronch@users.noreply.github.com>
2023-06-11 21:13:36 -0600

    slope movement done, started on smooth movement

---
+++ Assets/World/level_UdonProgramSources/MovingPlatform.cs
@@ -0,0 +1,32 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class MovingPlatform : UdonSharpBehaviour
+{
+    public Transform platform;
+    public Transform start, end;
+    public float speed;
+
+    private bool forward = true;
+
+    void Update()
+    {
+        if (forward)
+        {
+            platform.position = Vector3.MoveTowards(platform.position, end.position, speed * Time.deltaTime);
+        }
+        else 
+        {
+            platform.position = Vector3.MoveTowards(platform.position, start.position, speed * Time.deltaTime);
+        }
+        
+
+        if (platform.position == end.position || platform.position == start.position)
+        {
+            forward = !forward;
+        }
+    }
+}
--- Assets/World/level_UdonProgramSources/PlayerManager.cs
+++ Assets/World/level_UdonProgramSources/PlayerManager.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 using VRC.Udon.Common;
+using UnityEngine.UI;
 
 public class PlayerManager : UdonSharpBehaviour
 {
     public Transform spherePosition;
+    public Text debugText;
 
     public LayerMask groundLayers;
-    public float baseWalk = 5f, baseJump = 12f, baseGravity = 0.8f, flySpeed = 15f, sphereRadius = 1f, maxGroundedRise = 1f, maxSlope = 45f;
+    public float baseSpeed = 5f, baseJump = 12f, baseGravity = 0.8f, flySpeed = 15f, sphereRadius = 1f, maxGroundedRise = 1f, maxSlope = 45f, baseAcceleration = 0.2f, airAcceleration = 0f;
+    private float currentSpeed = 0f, currentAcceleration = 0f, maxSpeed;
 
     public VRCPlayerApi player;
 
@@ -26,11 +29,11 @@ public class PlayerManager : UdonSharpBehaviour
     void Start()
     {
         player = Networking.LocalPlayer;
-        player.SetWalkSpeed(baseWalk);
-        //player.SetRunSpeed(baseRun);
-        //player.SetStrafeSpeed(baseStrafe);
-        player.SetJumpImpulse(baseJump);
-        player.SetGravityStrength(baseGravity);
+        player.SetWalkSpeed(0);
+        player.SetRunSpeed(0);
+        player.SetStrafeSpeed(0);
+        player.SetJumpImpulse(0);
+        player.SetGravityStrength(0);
     }
 
     void Update()
@@ -39,10 +42,30 @@ public class PlayerManager : UdonSharpBehaviour
         {
             jumpDelayTimer += Time.deltaTime;
         }
+        if (coyoteTimer < maxCoyote)
+        {
+            coyoteTimer += Time.deltaTime;
+        }
+        if (bufferTimer < maxBuffer)
+        {
+            bufferTimer += Time.deltaTime;
+        }
+
+        debugText.text = "jumpDelayTimer: " + jumpDelayTimer.ToString();
+
+        if (onSlope && jumpDelayTimer >= maxJumpDelay)
+        {
+            player.SetVelocity(Vector3.ProjectOnPlane(moveVelocity, hitInfo.normal));
+        }
+        else
+        {
+            player.SetVelocity(moveVelocity);
+        }
     }
 
     void FixedUpdate()
     {
+        debugText.text = "";
         groundCheck();
         Jump();
         ApplyMovement();
@@ -51,9 +74,35 @@ public class PlayerManager : UdonSharpBehaviour
     void ApplyMovement()
     {
         // horizontal
-        moveDir = moveDir.normalized;
-        moveVelocity.x = (moveDir * baseWalk).x;
-        moveVelocity.z = (moveDir * baseWalk).z;
+        // if (moveDir.magnitude > 1f)
+        // {
+        //     moveDir = moveDir.normalized;
+        // }
+
+        // if (grounded)
+        // {
+        //     currentAcceleration = baseAcceleration;
+        // }
+        // else
+        // {
+        //     currentAcceleration = airAcceleration;
+        // }
+
+        // maxSpeed = baseSpeed * moveDir.magnitude;
+
+        // if (currentSpeed < maxSpeed)
+        // {
+        //     currentSpeed += currentAcceleration;
+        // }
+        // else if (currentSpeed > maxSpeed)
+        // {
+        //     currentSpeed -= currentAcceleration;
+        // }
+        float targetSpeed = moveDir.magnitude * baseSpeed;
+
+
+        moveVelocity.x = (moveDir * baseSpeed).x;
+        moveVelocity.z = (moveDir * baseSpeed).z;
         
         float playerRotation = Mathf.Deg2Rad * (player.GetRotation().eulerAngles.y);
 
@@ -68,7 +117,7 @@ public class PlayerManager : UdonSharpBehaviour
         {
             moveVelocity.y -= baseGravity;
         }
-        else if (jumpDelayTimer >= maxJumpDelay) // if on ground and jump safety delay is past due
+        else if (jumpDelayTimer >= maxJumpDelay && moveVelocity.y < 0f) // if on ground and jump safety delay is past due and moving down
         {
             moveVelocity.y = 0f;
         }
@@ -85,10 +134,12 @@ public class PlayerManager : UdonSharpBehaviour
 
     void Jump()
     {
-        if (jumping && grounded)
+        if (bufferTimer <= maxBuffer && coyoteTimer <= maxCoyote)
         {
             moveVelocity.y = baseJump;
             jumpDelayTimer = 0f;
+            bufferTimer = maxBuffer+1;
+            coyoteTimer = maxCoyote+1;
         }
     }
 
@@ -98,17 +149,14 @@ public class PlayerManager : UdonSharpBehaviour
 
         float angle = Vector3.Angle(transform.up, hitInfo.normal);
 
-        if (test)
+        if (test && (angle <= maxSlope) && (moveVelocity.y <= maxGroundedRise))
         { // grounded if not moving up too fast, touching ground, and slope not too high
-            if (angle <= maxSlope)
-            {
-                if (moveVelocity.y <= maxGroundedRise)
-                {
-                    grounded = false;
-                }
-            }
+            coyoteTimer = 0f;
+            grounded = true;
         }
-        Debug.Log(angle);
+        else{
+            grounded = false;
+        }
 
         onSlope = hitInfo.normal != Vector3.up;
     }
@@ -116,6 +164,11 @@ public class PlayerManager : UdonSharpBehaviour
     public override void InputJump(bool boolValue, UdonInputEventArgs args)
     {
         jumping = boolValue;
+
+        if (jumping)
+        {
+            bufferTimer = 0f;
+        }
 
         //flying = boolValue;
     }
@@ -130,9 +183,19 @@ public class PlayerManager : UdonSharpBehaviour
         moveDir.z = floatValue;
     }
 
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(spherePosition.position, sphereRadius);
-    }
+    // void OnDrawGizmos()
+    // {
+    //     if (grounded)
+    //     {
+    //         Gizmos.color = Color.green;
+    //         Debug.Log("green");
+    //     }
+    //     else
+    //     {
+    //         Gizmos.color = Color.red;
+    //         Debug.Log("red");
+    //     }
+        
+    //     Gizmos.DrawSphere(spherePosition.position - new Vector3(0, sphereRadius, 0), sphereRadius);
+    // }
 }
